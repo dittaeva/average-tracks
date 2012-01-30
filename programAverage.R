0364f223494b48a94119de3416ff12a23652471a
Guttorm Flatabø <post@guttormflatabo.com>
2012-01-30 14:17:22 +0100

    added input and output directories. Adapted programAverage.R to work from current directory. Cleaned up comments.

--- programAverage.R
+++ programAverage.R
@@ -4,19 +4,19 @@
 # lattitude: north-south; y
 rm(list=ls())
 
-# Directory where this file and "functionsGlobal.R" and "functionsAverage.R" are located.
-setwd("~/bin/average-tracks/") 
+# Working directory. Directory where programAverage.R and functions are located. Also where relative paths relate to. Uncomment and customise if needed, or current directory will be used.
+#setwd("~/bin/average-tracks/") 
 
 source("functionsGlobal.R")
 source("functionsAverage.R")
 
 loadedTracks <- fLoadFiles(
-	inputDir="~/input/", # Directory with input files. 
+	inputDir="input", # Directory with input files.
 	listOfFiles=NULL,
-	fileType="gpx", # Use "gpx" or "csv"
+	fileType="gpx", # "gpx" or "csv".
 	nTracks=0,
-	loadHeader=FALSE, # csv files without a header
-	method="rgdal") # Can use "rgdal" or "maptools". Both will autoinstall if you have the necessary libraries, and installation permissions.
+	loadHeader=FALSE, # csv files without a header.
+	method="rgdal") # "rgdal" or "maptools". Both will autoinstall if necessary libraries are present.
 
 fileNames <- loadedTracks[[2]]
 tracks <- loadedTracks[[1]]
@@ -25,10 +25,10 @@ averageTrack <- fAverageTrack(
 	tracks[tracks$routeID==1,], # track 1
 	tracks[tracks$routeID==2,], # track 2
 	elevation = TRUE,
-	method="b") # combine both averaging methods. "p" for only point method, "l" for line method.
+	method="b") # Combine both averaging methods. "p" for only point method, "l" for line method.
 
-# uncomment png() to save plot as png. !! See also dev.off() on line 42. !!
-#png(filename = "~/averageTrack.png", width=2048, height=2048, res=200)
+# Uncomment png() to save plot as png, also uncomment dev.off() on line 42.
+#png(filename = "output/averageTrack.png", width=2048, height=2048, res=200)
  plot(tracks[tracks$routeID==1,2:1], type="l", pch=2, col="green")
  lines(tracks[tracks$routeID==2,2:1], type="l", pch=2, col="green")
  lines(averageTrack[,2:1], type="l", pch=1, col="red")
@@ -39,11 +39,11 @@ averageTrack <- fAverageTrack(
  legend.pch <- c(2,1,4) 
  legend.position <- "topleft"
  legend(x=legend.position, legend=legend.text, col=legend.col, lty=legend.lty, pch=legend.pch)
-#dev.off() # uncomment when saving plot
+#dev.off() # Uncomment to save plot.
 
 fSaveTrack(
 	track=averageTrack,
-	outputDir="~/output", # Directory where to save the output file.
-	trk="20120121", # part of the trackname. "track" will be placed in front of "trk".
+	outputDir="output", # Directory where output file will be saved.
+	trk="20120121", # Part of the trackname. "track" will be placed in front of "trk" value.
 	roundTo=6)
 
